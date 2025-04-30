e19c017144e1aef2f5ba8ef6ef8efd9cbd95e916
thephez <thephez@users.noreply.github.com>
2025-04-30 12:40:27 -0400

    fix: use bigint for balance comparison

--- test/test.js
+++ test/test.js
@@ -174,7 +174,7 @@ describe(`Tutorial Code Tests (${new Date().toLocaleTimeString()})`, function su
       console.log(`\tRegistered identity: ${identity.toJSON().id}`);
       expect(identity).to.be.instanceOf(Identity);
       // New identity credit balance should be (10000 duffs - a small fee) * 1000
-      expect(identity.balance).to.be.above(newIdentityBalance);
+      assert.isTrue(identity.balance > BigInt(newIdentityBalance), 'identity balance is not above expected minimum');
       // assert.containsAllKeys(identity.toJSON(), ['id', 'publicKeys', 'balance', 'revision']);
     }).timeout(60000);
 
